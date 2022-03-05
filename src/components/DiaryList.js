62b3cd49883ec8479438eaac0546d0f18b1ffc58
Seungjoo Kim <sjskybone@gmail.com>
2022-03-04 17:46:27 -0800

    Added the getProcessedDiaryList function to bring up the right result for the filtering btn. #1

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -26,6 +26,20 @@ const ControlMenu = ({value, onChange, optionList}) => {
 const DiaryList = ({ diaryList }) => {
   const [sortType, setSortType] = useState('lastest');
 
+  const getProcessedDiaryList = () => {
+    const compare = (a, b) => {
+      if (sortType === 'latest') {
+        return parseInt(b.date) - parseInt(a.date);
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    }
+
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+    const sortedList = copyList.sort(compare);
+    return sortedList;
+  }
+
   return (
     <div>
       <ControlMenu
@@ -33,7 +47,7 @@ const DiaryList = ({ diaryList }) => {
         onChange={setSortType}
         optionList={sortOptionList}
       />
-      {diaryList.map((it) => (
+      {getProcessedDiaryList().map((it) => (
         <div key={it.id}>
           {it.content}
         </div>
