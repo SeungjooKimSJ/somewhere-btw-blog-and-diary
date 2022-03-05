a54fab3534928a9302fbf240136e11c1d1d58508
Seungjoo Kim <sjskybone@gmail.com>
2022-03-04 16:19:26 -0800

    Added decrease and increase month function on the arrow btns and checked it working fine. #1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,16 +5,26 @@ import Button from './../components/Button';
 
 const Home = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
-  console.log(currentDate);
-
   const headText = `${currentDate.getFullYear()}. ${currentDate.getMonth() + 1}.`
+
+  const increaseMonth = () => {
+    setCurrentDate(
+      new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, currentDate.getDate())
+    );
+  }
+
+  const decreaseMonth = () => {
+    setCurrentDate(
+      new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, currentDate.getDate())
+    );
+  }
 
   return (
     <div>
       <Header
         headText={headText}
-        leftChild={<Button text={'<'} onClick={() => {}} />}
-        rightChild={<Button text={'>'} onClick={() => {}} />}
+        leftChild={<Button text={'<'} onClick={decreaseMonth} />}
+        rightChild={<Button text={'>'} onClick={increaseMonth} />}
       />
     </div>
   );
