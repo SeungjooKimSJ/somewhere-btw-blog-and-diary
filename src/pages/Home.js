10b1c4558c92809737baead319aa39817f1cfde4
Seungjoo Kim <sjskybone@gmail.com>
2022-03-04 16:11:36 -0800

    Checked Header shows current time and month. #1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
+import { useState } from "react";
+
+import Header from './../components/Header';
+
 const Home = () => {
+  const [currentDate, setCurrentDate] = useState(new Date());
+  console.log(currentDate);
+
+  const headText = `${currentDate.getFullYear()}. ${currentDate.getMonth() + 1}.`
+
   return (
     <div>
-      <h1>Home</h1>
-      <p>This is Home page.</p>
+      <Header headText={headText} />
     </div>
   );
 };
