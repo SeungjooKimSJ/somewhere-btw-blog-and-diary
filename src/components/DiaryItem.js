2675d5f9e74959315fbc07b5af88169c68c826c2
Seungjoo Kim <sjskybone@gmail.com>
2022-03-05 11:46:29 -0800

    Added Edit btn on the diary list. #1

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -1,3 +1,5 @@
+import Button from "./Button";
+
 const DiaryItem = ({ id, emotion, content, date }) => {
   const strDate = new Date(parseInt(date)).toLocaleDateString();
 
@@ -18,6 +20,9 @@ const DiaryItem = ({ id, emotion, content, date }) => {
         <div className="diary_content_preview">
           {content.slice(0, 25)}
         </div>
+      </div>
+      <div className="btn_container">
+        <Button text={'Edit'} />
       </div>
       <div>
 
