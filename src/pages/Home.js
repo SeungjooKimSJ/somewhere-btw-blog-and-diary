afadade6d9315a32f2ce70009585b948c2de3a15
Seungjoo Kim <sjskybone@gmail.com>
2022-03-04 16:48:34 -0800

    Checked if diary contents only correspond to each month using the filter method. #1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { DiaryStateContext } from "../App";
 
 import Header from './../components/Header';
@@ -7,8 +7,34 @@ import Button from './../components/Button';
 const Home = () => {
   const diaryList = useContext(DiaryStateContext);
 
+  const [data, setData] = useState([]);
   const [currentDate, setCurrentDate] = useState(new Date());
   const headText = `${currentDate.getFullYear()}. ${currentDate.getMonth() + 1}.`
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const firstDay = new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth(),
+        1
+      ).getTime();
+
+      const lastDay = new Date(
+        currentDate.getFullYear(),
+        currentDate.getMonth() + 1,
+        0
+      ).getTime();
+
+      setData(
+        diaryList.filter((it) => firstDay <= it.date && it.date <= lastDay)
+      );
+    }
+  }, [diaryList, currentDate]);
+
+
+  useEffect(() => {
+    console.log(data);
+  }, [data])
 
   const increaseMonth = () => {
     setCurrentDate(
