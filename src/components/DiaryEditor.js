9a8119d4f99da48c1ae749743d2ecde7bcf78ec8
Seungjoo Kim <sjskybone@gmail.com>
2022-03-06 02:21:24 -0800

    Added emotion's with styling on the new diary page. #2

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -3,6 +3,35 @@ import { useNavigate } from "react-router-dom";
 
 import Header from "./Header";
 import Button from "./Button";
+import EmotionItem from "./EmotionItem";
+
+const emotionList = [
+  {
+    emotion_id: 1,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
+    emotion_descript: 'Fantastic'
+  },
+  {
+    emotion_id: 2,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
+    emotion_descript: 'Good'
+  },
+  {
+    emotion_id: 3,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
+    emotion_descript: 'So So'
+  },
+  {
+    emotion_id: 4,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
+    emotion_descript: 'Bad'
+  },
+  {
+    emotion_id: 5,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
+    emotion_descript: 'Terrible'
+  }
+];
 
 const getStrDate = (date) => {
   return date.toISOString().slice(0, 10);
@@ -31,6 +60,14 @@ const DiaryEditor = () => {
               onChange={(e) => setDate(e.target.value)}
               type='date'
             />
+          </div>
+        </section>
+        <section>
+          <h4>Today's Feeling</h4>
+          <div className="input_box emotion_list_container">
+            {emotionList.map((it) => (
+              <EmotionItem key={it.emotion_id} {...it} />
+            ))}
           </div>
         </section>
       </div>
