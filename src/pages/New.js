532fdbca7b3d248fd9a28a7296ccebccfed913f5
Seungjoo Kim <sjskybone@gmail.com>
2022-03-08 10:40:13 -0800

    Checked each page's title is changing. #11

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import DiaryEditor from "../components/DiaryEditor";
 
 const New = () => {
+  useEffect(() => {
+    const titleElement = document.getElementsByTagName('title')[0];
+    titleElement.innerHTML = `Write New Diary&Blog`;
+  }, []);
+
   return (
     <div>
       <DiaryEditor />
